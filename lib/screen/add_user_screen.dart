6b2233947369fae3a273584d2d674c5512227d19
olizyusuf <olizyusuf22@gmail.com>
2025-06-10 22:39:20 +0700

    design add user

--- lib/screen/add_user_screen.dart
+++ lib/screen/add_user_screen.dart
@@ -11,22 +11,82 @@ class AddUserScreen extends StatelessWidget {
 
     DisplayHelper displayHelper = DisplayHelper();
 
+    List<String> roles = ['Administrator', 'Kasir', 'Manager'];
+    String defaultRole = 'Administrator';
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
       body: Container(
-        padding: const EdgeInsets.all(5.0),
+        padding: const EdgeInsets.only(
+          top: 10.0,
+          left: 8.0,
+          right: 8.0,
+        ),
         width: displayHelper.widthDp(context),
         child: SingleChildScrollView(
           child: Column(
             children: [
               TextField(
                 decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(),
-                    hintText: 'Username',
-                    fillColor: Colors.black),
+                  border: OutlineInputBorder(),
+                  hintText: 'Username',
+                  hintStyle: TextStyle(color: Colors.black26),
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  labelText: 'Username',
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.black26),
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  labelText: 'Password',
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Re-Password',
+                  hintStyle: TextStyle(color: Colors.black26),
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  labelText: 'Re-Password',
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              DropdownButton(
+                value: defaultRole,
+                items: roles.map((String value) {
+                  return DropdownMenuItem(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  defaultRole = value.toString();
+                },
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Simpan'),
               ),
             ],
           ),
